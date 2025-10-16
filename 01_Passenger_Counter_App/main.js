1db503e9a11ee95408e5fdb61526832d846e14c8
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-10-16 08:46:12 +0500

    Commented main.js in Passenger counter

--- 01_Passenger_Counter_App/main.js
+++ 01_Passenger_Counter_App/main.js
@@ -1,18 +1,24 @@
+// selecting the html element by their 'id' attribute
 let countEl = document.getElementById('count-el');
 let saveEl = document.getElementById('save-el')
 
+// initializing a variable to 0
 let count = 0;
 
+// defining increment function (this function increases the count value by 1)
 function increment() {
     count += 1;
     countEl.innerText = count;
 }
 
+// defining reset count function (this function will reset the count value to 0)
 function resetCount() {
     count = 0;
     countEl.textContent = count;
 }
 
+// defining save function 
+// (this function will save the current session count value and display in an html element selected by 'id')
 function save() {
     let countStr = count + " - ";
     saveEl.textContent += countStr;
