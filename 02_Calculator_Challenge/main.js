5fb65c3f6d8d4681cb160b921eb1dc191ae7fe54
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-10-16 08:53:41 +0500

    Commented main.js in Calculator Project

--- 02_Calculator_Challenge/main.js
+++ 02_Calculator_Challenge/main.js
@@ -1,36 +1,36 @@
+// initiazlizing two variables
 let num1 = 8
 let num2 = 2
+
+// selecting an html element by their 'id' attribute and changing their text using 'textContent' property of 'JS'
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
-// Create four functions: add(), subtract(), divide(), multiply()
-// Call the correct function when the user clicks on one of the buttons
-// Perform the given calculation using num1 and num2
-// Render the result of the calculation in the paragraph with id="sum-el"
-
-// E.g. if the user clicks on the "Plus" button, you should render
-// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
-
+// selecting an html element by their 'id' attribute
 resultElement = document.getElementById('result-el');
 
+// defining the add function to perform the required action
 function add() {
     let addElementText = document.getElementById('add-el').innerText;
     let result = num1 + num2;
     resultElement.textContent = addElementText + ": " + result;
 }
 
+// defining the subtract function to perform the required action
 function subtract() {
     let subElementText = document.getElementById('sub-el').innerText;
     let result = num1 - num2;
     resultElement.textContent = subElementText + ": " + result;
 }
 
+// defining the multiplication function to perform the required action
 function multiply() {
     let mulElementText = document.getElementById('mul-el').innerText;
     let result = num1 * num2;
     resultElement.textContent = mulElementText + ": " + result;
 }
 
+// defining the division function to perform the required action
 function divide() {
     let divElementText = document.getElementById('div-el').innerText;
     let result = num1 / num2;
